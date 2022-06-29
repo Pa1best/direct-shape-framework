e570ffc47dbd2852ad0d098773ca9c7ef61c7c8b
Alex Slutski <slutski.alex@gmail.com>
2022-06-29 10:59:32 +0300

    feat: minor fixes & all public methods commented

--- DirectShapeFramework/Highlight.cs
+++ DirectShapeFramework/Highlight.cs
@@ -5,12 +5,21 @@ using DirectShapeFramework.Utils;
 
 namespace DirectShapeFramework;
 
+/// <summary>
+/// I will helps you to highlight invisible Geometry
+/// </summary>
 public static class Highlight
 {
     private static View3D _defaultView;
     private static readonly string _defaultMarkPrefix = "DSF";
     private static readonly string _defaultViewName = "DSF View";
 
+    /// <summary>
+    /// Helps to highlight Geometry presented by single GeometryObject
+    /// </summary>
+    /// <param name="doc">Revit Document you are working on</param>
+    /// <param name="geometryObject">GeometryObject to highlight</param>
+    /// <returns>DirectShape object</returns>
     public static DirectShape Geometry(Document doc, GeometryObject geometryObject)
     {
         //create direct shape and assign the sphere shape
@@ -21,6 +30,12 @@ public static class Highlight
         return ds;
     }
 
+    /// <summary>
+    /// Helps to highlight Geometry presented by List of GeometryObjects
+    /// </summary>
+    /// <param name="doc">Revit Document you are working on</param>
+    /// <param name="geometryObjects">GeometryObjects to highlight</param>
+    /// <returns>DirectShape object</returns>
     public static DirectShape Geometry(Document doc, IList<GeometryObject> geometryObjects)
     {
         var ds = DirectShape.CreateElement(doc, new ElementId(BuiltInCategory.OST_GenericModel));
@@ -30,14 +45,30 @@ public static class Highlight
         return ds;
     }
 
+    /// <summary>
+    /// Helps to highlight Rectangular Prism oriented in XYZ(0,0,0) coordinates
+    /// </summary>
+    /// <param name="doc">Revit Document you are working on</param>
+    /// <param name="prismCenter">Central point of Prism. Is also is a location point</param>
+    /// <param name="width"></param>
+    /// <param name="depth"></param>
+    /// <param name="height"></param>
+    /// <returns>DirectShape object</returns>
     [UsedImplicitly]
-    public static DirectShape RectangularPrism(Document doc, XYZ prismCenter, double d1, double d2, double d3)
+    public static DirectShape RectangularPrism(Document doc, XYZ prismCenter, double width, double depth, double height)
     {
-        var prism = GeometryUtils.CreateRectangularPrism(prismCenter, d1, d2, d3);
+        var prism = GeometryUtils.CreateRectangularPrism(prismCenter, width, depth, height);
 
         return Geometry(doc, prism);
     }
 
+    /// <summary>
+    /// Helps to highlight Cube oriented in XYZ(0,0,0) coordinates
+    /// </summary>
+    /// <param name="doc">Revit Document you are working on</param>
+    /// <param name="center">Central point of Cube. Is also is a location point</param>
+    /// <param name="edgeLength"></param>
+    /// <returns>DirectShape object</returns>
     [UsedImplicitly]
     public static DirectShape Cube(Document doc, XYZ center, double edgeLength)
     {
@@ -45,6 +76,13 @@ public static class Highlight
         return Geometry(doc, prism);
     }
 
+    /// <summary>
+    /// Helps to highlight Sphere
+    /// </summary>
+    /// <param name="doc">Revit Document you are working on</param>
+    /// <param name="center">Central point of Sphere. Is also is a location point</param>
+    /// <param name="radius"></param>
+    /// <returns>DirectShape object</returns>
     [UsedImplicitly]
     public static DirectShape Sphere(Document doc, XYZ center, double radius)
     {
@@ -52,6 +90,13 @@ public static class Highlight
         return Geometry(doc, prism);
     }
 
+    /// <summary>
+    /// Helps to highlight element's Face
+    /// </summary>
+    /// <param name="doc">Revit Document you are working on</param>
+    /// <param name="element">Host element</param>
+    /// <param name="face">Face of host element to highlight</param>
+    /// <returns>DirectShape object</returns>
     [UsedImplicitly]
     public static DirectShape Face(Document doc, Element element, Face face)
     {
@@ -59,6 +104,13 @@ public static class Highlight
         return Geometry(doc, g);
     }
 
+    /// <summary>
+    /// Creates a list of DirectShape spheres oriented according to chosen vector with chosen origin point
+    /// </summary>
+    /// <param name="doc">Revit Document you are working on</param>
+    /// <param name="vector">Vector to highlight</param>
+    /// <param name="start">Start point of this vector</param>
+    /// <returns>List of DirectShape objects</returns>
     [UsedImplicitly]
     public static List<DirectShape> Vector(Document doc, XYZ vector, XYZ start)
     {
@@ -68,12 +120,26 @@ public static class Highlight
         return curveShapes;
     }
 
+    /// <summary>
+    /// Creates a DirectShape sphere with some radius (by default 0.3 feet)
+    /// </summary>
+    /// <param name="doc">Revit Document you are working on</param>
+    /// <param name="point"></param>
+    /// <param name="radius"></param>
+    /// <returns>DirectShape object</returns>
     public static DirectShape Point(Document doc, XYZ point, double radius = 0.3)
     {
         var g = GeometryUtils.CreateSphere(point, radius);
         return Geometry(doc, g);
     }
 
+    /// <summary>
+    /// Creates a number of Direct Shape spheres with preselected steps (by default 0.01 feet)
+    /// </summary>
+    /// <param name="doc">Revit Document you are working on</param>
+    /// <param name="curve"></param>
+    /// <param name="step"></param>
+    /// <returns>List of DirectShape objects</returns>
     public static List<DirectShape> CurveByPoints(Document doc, Curve curve, double step = 0.01)
     {
         var points = new List<XYZ>();
@@ -84,6 +150,12 @@ public static class Highlight
         return result;
     }
 
+    /// <summary>
+    /// Helps to highlight BoundingBox of some element
+    /// </summary>
+    /// <param name="doc">Revit Document you are working on</param>
+    /// <param name="element">Element to retrieve Bounding Box</param>
+    /// <returns>DirectShape object</returns>
     [UsedImplicitly]
     public static DirectShape BoundingBox(Document doc, Element element)
     {
@@ -91,6 +163,12 @@ public static class Highlight
         return BoundingBox(doc, bBox);
     }
 
+    /// <summary>
+    /// Helps to highlight BoundingBox 
+    /// </summary>
+    /// <param name="doc">Revit Document you are working on</param>
+    /// <param name="bBox">BoundingBox to highlight</param>
+    /// <returns>DirectShape object</returns>
     [CanBeNull]
     public static DirectShape BoundingBox(Document doc, BoundingBoxXYZ bBox)
     {
@@ -104,6 +182,10 @@ public static class Highlight
         return Geometry(doc, solid);
     }
 
+    /// <summary>
+    /// Removes all DirectShape elements and DSF default view from project
+    /// </summary>
+    /// <param name="doc">Revit Document you are working on</param>
     [UsedImplicitly]
     public static void ClearAll(Document doc)
     {
@@ -111,25 +193,14 @@ public static class Highlight
         doc.Delete(_defaultView?.Id);
     }
 
-    [CanBeNull]
-    private static string GenerateMark(Document doc)
-    {
-        var existingElements = new FilteredElementCollector(doc).OfClass(typeof(DirectShape)).ToElements();
-        var parameters = existingElements.Select(x => x.get_Parameter(BuiltInParameter.ALL_MODEL_MARK)?.AsString()).ToList();
-        var index = parameters.Count;
-        string mark;
-        do
-        {
-            mark = _defaultMarkPrefix + index++;
-        } while (parameters.Contains(mark));
-
-        return mark;
-    }
-
+    /// <summary>
+    /// Creates a special 3D View with predefined filters to show all DirectShapes with some color
+    /// </summary>
+    /// <param name="uiDoc"></param>
     [UsedImplicitly]
     public static void OnView3D(UIDocument uiDoc)
     {
-        if (_defaultView == null)
+        if (_defaultView is not {IsValidObject: true})
         {
             var doc = uiDoc.Document;
             var existingView = ViewUtils.GetView3DByName(doc, _defaultViewName);
@@ -145,5 +216,20 @@ public static class Highlight
             }
         }
         uiDoc.ActiveView = _defaultView;
+    }
+
+    [CanBeNull]
+    private static string GenerateMark(Document doc)
+    {
+        var existingElements = new FilteredElementCollector(doc).OfClass(typeof(DirectShape)).ToElements();
+        var parameters = existingElements.Select(x => x.get_Parameter(BuiltInParameter.ALL_MODEL_MARK)?.AsString()).ToList();
+        var index = parameters.Count;
+        string mark;
+        do
+        {
+            mark = _defaultMarkPrefix + index++;
+        } while (parameters.Contains(mark));
+
+        return mark;
     }
 }
